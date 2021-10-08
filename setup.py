8594a395a080f126dec0c857ac7f796ac6a8b168
Nate <nathan.smeltzer@gmail.com>
2021-10-08 13:55:26 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ebay-oauth",
+    packages=setuptools.find_packages(),
+    version="0.0.1",
+    description="eBay Oauth Client",
+    long_description=long_description,
+    author="Market Math LLC",
+    author_email="nathan.smeltzer@gmail.com",
+    url="https://gitlab.com/Smeltzer/ebay-oauth",
+    download_url="https://gitlab.com/Smeltzer/ebay-oauth/-/archive/master/selenium-browser-shopper-master.zip",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: Other/Proprietary License",
+    ],
+    python_requires='>=3.6',)
