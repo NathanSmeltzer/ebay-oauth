c1c4dfe3024e5c98f46cef9e33679f0dc612dbeb
Nate <nathan.smeltzer@gmail.com>
2024-02-20 07:56:07 -0500

    attempting shared logger

--- oauthclient/__init__.py
+++ oauthclient/__init__.py
@@ -1,3 +1,10 @@
 import logging
 from decouple import config
+logger_name = config("LOGGER_NAME", default="ebay")
 logger = logging.getLogger(config("LOGGER_NAME", default="ebay")).setLevel(config("LOG_LEVEL", default="INFO"))
+# make the logger if it doesn't exist
+if not logger:
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(config("LOG_LEVEL", default="INFO"))
+    logger.addHandler(logging.FileHandler(filename=logger_name + ".log"))
+    logger.info("logger created")
