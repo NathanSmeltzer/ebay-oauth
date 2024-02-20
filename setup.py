4be17e97f756b10248224afd54dea25f36f45d9d
Nate <nathan.smeltzer@gmail.com>
2024-02-20 09:47:05 -0500

    fix description setup

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import setuptools
 
-with open("README.adoc", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="ebay-oauth",
     packages=setuptools.find_packages(),
     version="0.0.7",
     description="eBay Oauth Client ShipAware Fork",
-    long_description=long_description,
     author="Logistero Ltd dba ShipAware",
     author_email="nathan.smeltzer@gmail.com",
     url="https://github.com/NathanSmeltzer/ebay-oauth",
